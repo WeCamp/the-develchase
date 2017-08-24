a75cad3a8a3768e5af273e290f1e91c8e19cd835
Holger Woltersdorf <hw@hollo.me>
2017-08-24 14:59:59 +0200

    Refactors command to create example data

--- src/Application/CLI/Commands/CreateExampleDataCommand.php
+++ src/Application/CLI/Commands/CreateExampleDataCommand.php
@@ -8,14 +8,8 @@ namespace WeCamp\TheDevelChase\Application\CLI\Commands;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use triagens\ArangoDb\Collection as Collection;
-use triagens\ArangoDb\CollectionHandler as CollectionHandler;
-use triagens\ArangoDb\Statement;
-use WeCamp\TheDevelChase\Application\Documents\Conference;
-use WeCamp\TheDevelChase\Application\Documents\Topic;
+use WeCamp\TheDevelChase\Application\Collections\CollectionRepository;
 use WeCamp\TheDevelChase\Application\Documents\User;
-use WeCamp\TheDevelChase\Application\Interfaces\DocumentInterface;
-use WeCamp\TheDevelChase\Env;
 
 /**
  * Class CreateExampleDataCommand
@@ -23,79 +17,8 @@ use WeCamp\TheDevelChase\Env;
  */
 final class CreateExampleDataCommand extends AbstractCommand
 {
-	private $connection;
-
-	private $testdata;
-
-	public function __construct( $name, Env $env )
-	{
-		parent::__construct( $name, $env );
-
-		$this->connection = $env->getArangoConnection();
-
-		// Example data structure
-		$this->testdata = [
-			'users'       => [
-				[
-					'firstName' => 'Tom',
-					'lastName'  => 'Riddle',
-					'topics'    => [
-						'PHP', 'Docker',
-					],
-				],
-				[
-					'firstName' => 'Mary',
-					'lastName'  => 'Poppins',
-					'topics'    => [
-						'JavaScript', 'Vagrant',
-					],
-				],
-				[
-					'firstName' => 'Jennis',
-					'lastName'  => 'Joplin',
-					'topics'    => [
-						'PHP', 'Vagrant',
-					],
-				],
-				[
-					'firstName' => 'Elvis',
-					'lastName'  => 'Presley',
-					'topics'    => [
-						'JavaScript', 'Docker', 'PHP',
-					],
-				],
-
-			],
-			'topics'      => [
-				[
-					'name' => 'PHP',
-				],
-				[
-					'name' => 'JavaScript',
-				],
-				[
-					'name' => 'Docker',
-				],
-				[
-					'name' => 'Vagrant',
-				],
-			],
-			'conferences' => [
-				[
-					'name'     => 'PHP North West',
-					'location' => 'Northwest, UK',
-				],
-				[
-					'name'     => 'PHP South Coast',
-					'location' => 'South Coast, USA',
-				],
-				[
-					'name'     => 'PHP Yorkshire',
-					'location' => 'Yorkshire, UK',
-				],
-			],
-		];
-	}
+	/** @var SymfonyStyle */
+	private $style;
 
 	protected function configure() : void
 	{
@@ -104,80 +27,38 @@ final class CreateExampleDataCommand extends AbstractCommand
 
 	protected function execute( InputInterface $input, OutputInterface $output ) : int
 	{
-		$style = new SymfonyStyle( $input, $output );
+		$this->style = new SymfonyStyle( $input, $output );
+		$this->style->title( 'Adding test data.' );
 
-		$style->title( 'Adding test data.' );
+		$testData = require __DIR__ . '/../../../resources/ExampleData.php';
 
-		$collectionHandler = new CollectionHandler( $this->connection );
+		$collectionRepository = new CollectionRepository( $this->getEnv()->getArangoConnection() );
+		$this->createCollections( $collectionRepository, 'users', 'topics', 'conferences' );
 
-		foreach($this->testdata as $collectionName => $documents) {
-		    switch($collectionName) {
-                case 'users':
-                    $documentClassName = User::class;
-                    break;
-                case 'topics':
-                    $documentClassName = Topic::class;
-                    break;
-                case 'conferences':
-                    $documentClassName = Conference::class;
-                    break;
-                default:
-                    throw new \Exception('Unknown collection name');
-            }
+		foreach ( (array)$testData['users'] as $userData )
+		{
+			/** @var User $user */
+			$user = User::fromArray( $userData );
 
-            // Create a new collection
-            $style->section( 'Creating collection ' . $collectionName );
+			$collectionRepository->insertDocuments( 'users', $user );
+			$collectionRepository->insertDocuments( 'topics', ...$user->getTopics() );
+			$collectionRepository->insertDocuments( 'conferences', ...$user->getConferences() );
+		}
 
-            $collection = new Collection( $collectionName );
-
-            // Drops an existing collection with the same name
-            if ( $collectionHandler->has( $collection ) )
-            {
-                $collectionHandler->drop( $collection );
-            }
-
-            $collectionId = $collectionHandler->create( $collection );
-
-            $style->writeln( 'Created collection ' . $collection . ' with ID: ' . $collectionId );
-
-            $query = "INSERT @document INTO @@collectionName RETURN NEW";
-
-
-            foreach($documents as $data) {
-		        /** @var DocumentInterface $documentClassName */
-		        $document = $documentClassName::fromArray($data);
-
-                $statement = new Statement(
-                    $this->getEnv()->getArangoConnection(),
-                    [
-                        'query'     => $query,
-                        'count'     => true,
-                        'batchSize' => 1,
-                        'sanatize'  => true,
-                        'bindVars'  => [
-                            'document' => $document->toArray(),
-                            '@collectionName' => $collectionName
-                        ],
-                    ]
-                );
-
-                $statement->execute();
-            }
-        }
-//
-//		try
-//		{
-//
-//		}
-//		catch ( \Throwable $e )
-//		{
-//			$style->error( $e->getMessage() );
-//
-//			return 1;
-//		}
-//
-		$style->success( 'Data successfully added.' );
+		$this->style->success( '√ Data successfully added.' );
 
 		return 0;
+	}
+
+	private function createCollections( CollectionRepository $collectionRepository, string ...$collectionNames ) : void
+	{
+		foreach ( $collectionNames as $collectionName )
+		{
+			$this->style->section( 'Creating collection ' . $collectionName );
+
+			$collectionId = $collectionRepository->create( $collectionName );
+
+			$this->style->writeln( '<fg=green>√ CollectionRepository-ID: ' . $collectionId . '</>' );
+		}
 	}
 }
