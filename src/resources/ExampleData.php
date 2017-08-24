77780935c44b900f6fc834d479c4f95af34a6993
Holger Woltersdorf <hw@hollo.me>
2017-08-24 14:42:30 +0200

    Moves example data to separate file

---
+++ src/resources/ExampleData.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+/**
+ * @author hollodotme
+ */
+
+return [
+	'users' => [
+		[
+			'firstName'   => 'Tom',
+			'lastName'    => 'Riddle',
+			'topics'      => [
+				'PHP', 'Docker',
+			],
+			'conferences' => [
+				'PHP North West', 'PHP South Coast',
+			],
+		],
+		[
+			'firstName'   => 'Mary',
+			'lastName'    => 'Poppins',
+			'topics'      => [
+				'JavaScript', 'Vagrant',
+			],
+			'conferences' => [
+				'PHP South Coast', 'PHP Yorkshire',
+			],
+		],
+		[
+			'firstName'   => 'Jennis',
+			'lastName'    => 'Joplin',
+			'topics'      => [
+				'PHP', 'Vagrant',
+			],
+			'conferences' => [
+				'PHP North West', 'PHP Yorkshire',
+			],
+		],
+		[
+			'firstName'   => 'Elvis',
+			'lastName'    => 'Presley',
+			'topics'      => [
+				'JavaScript', 'Docker', 'PHP',
+			],
+			'conferences' => [
+				'PHP South Coast', 'PHP South Coast',
+			],
+		],
+
+	],
+];
