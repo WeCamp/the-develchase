a18c0d22eb654252aae38a081a2cab220d51b74c
Michael Harris <michaelh@magmadigital.co.uk>
2017-08-25 11:14:43 +0100

    Move API call data into local variable.

--- src/resources/public/frontend/src/App.js
+++ src/resources/public/frontend/src/App.js
@@ -2,57 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// class App extends Component {
-//     constructor() {
-//         super();
-//         this.state = { items: [] };
-//     }
-//
-//     componentDidMount() {
-//         fetch(`https://httpbin.org/get`)
-//             .then(result => {
-//                 this.setState({items:result.json()});
-//             });
-//
-//         console.log('hello');
-//         console.log(this.state.items);
-//     }
-//
-//     render() {
-//         return (
-//             <div></div>
-//         );
-        // return(
-        //     <div>
-        //         <div>Items:</div>
-        //         { this.state.items.map(item=> { return <div>{item.name}</div>}) }
-        //     </div>
-        // );
-//     }
-// }
-
-// export default App;
-
-
 export default class App extends React.Component {
     constructor() {
         super();
         this.state = { items: [] };
+
     }
 
     componentDidMount() {
-        fetch(`http://echo.jsontest.com/key/value/one/two`)
-            .then(result => {
-                this.setState({items:result.json()});
-                console.log(this.state);
+        var self = this;
+
+        fetch(`http://echo.jsontest.com/fruit/banana/apple/tree`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(parsedData) {
+                self.setState({items:parsedData});
+                // resolve(parsedData);
+                // console.log(parsedData);
+                console.log(self.state);
             });
-
-
     }
 
     render() {
-        console.log('hello');
-
         return(
              <div>
                  <div>Items:</div>
