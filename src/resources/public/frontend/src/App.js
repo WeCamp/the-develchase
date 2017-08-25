1bb692b5d8a2ce788ba7c7bb82e83265a9a4e21e
Thijs Hulshof <thijsh@yoast.com>
2017-08-25 09:41:32 +0200

    Frontend - data not yet connected

--- src/resources/public/frontend/src/App.js
+++ src/resources/public/frontend/src/App.js
@@ -1,18 +1,92 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import TopicSelector from "./Topics";
+import { ConferencesResult } from "./ConferencesResult";
+
+const Header = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin: auto;
+    margin-bottom: 0;
+    background-color: #4ABDAC;
+    height: 4em;
+    `;
+
+const Title = styled.h1`
+    color: #DFDCE3;
+`;
+
+const SubTitle = styled.h2`
+    color: #FC4A1A;
+`;
+
+const NavigationBar = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    margin-top: 0;
+    background-color: #FC4A1A;
+    border: 1px solid black;
+    height: 2em;
+    `;
+
+const MenuItem = styled.div`
+    width: 200px;
+    // margin: auto;
+    color: white;
+    &:hover {
+        color: black;
+        }
+    `;
+
+const Content = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    width: 100%;
+    background-color: white;
+    `;
+
+const Topics = styled.div`
+    width: 45%;
+    background-color: lightgrey;
+    margin: 2px;
+    `;
+
+const Conferences = styled.div`
+    width: 45%;
+    background-color: lightgrey;
+    margin: 2px;
+    `;
 
 class App extends Component {
+
   render() {
-    return (
+	  return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <Title>The DevelChasers</Title>
+        </Header>
+          <NavigationBar>
+              <MenuItem>Select your conferences</MenuItem>
+              <MenuItem>People to meet</MenuItem>
+          </NavigationBar>
+        <Content>
+            <Topics>
+                <SubTitle>Select your topics</SubTitle>
+                <TopicSelector />
+            </Topics>
+            <Conferences>
+                <SubTitle>We recommend visiting these conferences:</SubTitle>
+                <ConferencesResult></ConferencesResult>
+            </Conferences>
+        </Content>
       </div>
     );
   }
