962e676e65114b7ac3d39f5600dbf12f8116c0cf
Holger Woltersdorf <hw@hollo.me>
2017-08-25 10:41:38 +0200

    Re-groups the CLI commands, adds data:drop command

--- bin/cli.php
+++ bin/cli.php
@@ -8,6 +8,7 @@ namespace WeCamp\TheDevelChase\Bin;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Exception\RuntimeException;
 use WeCamp\TheDevelChase\Application\CLI\Commands\CreateExampleDataCommand;
+use WeCamp\TheDevelChase\Application\CLI\Commands\DropDataCommand;
 use WeCamp\TheDevelChase\Application\CLI\Commands\ImportDataCommand;
 use WeCamp\TheDevelChase\Env;
 
@@ -18,8 +19,9 @@ $app = new Application( 'TheDevelChase', '0.1.0' );
 
 try
 {
-	$app->add( new CreateExampleDataCommand( 'create:example-data', $env ) );
-	$app->add( new ImportDataCommand( 'import:data', $env ) );
+	$app->add( new CreateExampleDataCommand( 'data:create-examples', $env ) );
+	$app->add( new ImportDataCommand( 'data:import', $env ) );
+	$app->add( new DropDataCommand( 'data:drop', $env ) );
 
 	$exitCode = $app->run();
 
