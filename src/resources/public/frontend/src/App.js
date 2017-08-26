7faae0d74a38f6c9dcf6e6be528cccd0f2ca0237
Thijs Hulshof <thijsh@yoast.com>
2017-08-26 10:02:13 +0200

    Small adjustments

--- src/resources/public/frontend/src/App.js
+++ src/resources/public/frontend/src/App.js
@@ -84,49 +84,64 @@ export const TopicCheckbox = styled.input`
 	margin-right: 0 0 auto;
 `;
 
-const AllTopics = [{
-	id: 1,
-	name: 'Angular',
-	checked: true,
-}, {
-	id: 2,
-	name: 'PHP',
-	checked: false,
-}, {
-	id: 3,
-	name: 'Docker',
-	checked: false,
-}, {
-	id: 6,
-	name: 'JavaScript',
-	checked: false,
-}, {
-	id: 4,
-	name: 'React',
-	checked: false,
-}, {
-	id: 5,
-	name: 'Something else',
-	checked: false,
-}];
+// const AllTopics = [{
+// 	id: 1,
+// 	label: 'Scrum',
+// 	name: 'scrum',
+// 	checked: true,
+// }, {
+// 	id: 2,
+// 	label: 'PHP',
+// 	name: 'php',
+// 	checked: false,
+// }, {
+// 	id: 3,
+// 	label: 'Docker',
+// 	name: 'docker',
+// 	checked: false,
+// }, {
+// 	id: 6,
+// 	label: 'JavaScript',
+// 	name: 'javascript',
+// 	checked: false,
+// }, {
+// 	id: 4,
+// 	label: 'ReactJS',
+// 	name: 'react',
+// 	checked: false,
+// }, {
+// 	id: 5,
+// 	label: 'Symfony',
+// 	name: 'symfony',
+// 	checked: false,
+// }, {
+// 	id: 7,
+// 	label: 'ArangoDB',
+// 	name: 'arangodb',
+// 	checked: false,
+// }, {
+// 	id: 8,
+// 	label: 'Databases',
+// 	name: 'databases',
+// 	checked: false,
+// }, {
+// 	id: 9,
+// 	label: 'Security',
+// 	name: 'security',
+// 	checked: false,
+// }, {
+// 	id: 10,
+// 	label: 'Testing',
+// 	name: 'testing',
+// 	checked: false,
+// }];
+
+const AllTopics = [{"_key":"scrum","_id":"topics/scrum","_rev":"_VfiBDuy---","label":"scrum","name":"scrum"},{"_key":"mysql","_id":"topics/mysql","_rev":"_VfiBE_G---","label":"mysql","name":"mysql"},{"_key":"apis","_id":"topics/apis","_rev":"_VfiBDwq---","label":"apis","name":"apis"},{"_key":"bdd","_id":"topics/bdd","_rev":"_VfiBDpS---","label":"bdd","name":"bdd"},{"_key":"symfony","_id":"topics/symfony","_rev":"_VfiBE-2---","label":"symfony","name":"symfony"},{"_key":"ddd","_id":"topics/ddd","_rev":"_VfiBDpK---","label":"ddd","name":"ddd"},{"_key":"html","_id":"topics/html","_rev":"_VfiBEPK---","label":"html","name":"html"},{"_key":"rancher","_id":"topics/rancher","_rev":"_VfiBD8m---","label":"rancher","name":"rancher"},{"_key":"lamp","_id":"topics/lamp","_rev":"_VfiBEPe---","label":"lamp","name":"lamp"},{"_key":"architecture","_id":"topics/architecture","_rev":"_VfiBEJy---","label":"architecture","name":"architecture"},{"_key":"arangodb","_id":"topics/arangodb","_rev":"_VfiBE_O---","label":"arangodb","name":"arangodb"},{"_key":"docker","_id":"topics/docker","_rev":"_VfiBEPa---","label":"docker","name":"docker"},{"_key":"react","_id":"topics/react","_rev":"_VfiBELe---","label":"react","name":"react"},{"_key":"databases","_id":"topics/databases","_rev":"_VfiBEIa---","label":"databases","name":"databases"},{"_key":"testing","_id":"topics/testing","_rev":"_VfiBEBe---","label":"testing","name":"testing"},{"_key":"json","_id":"topics/json","_rev":"_VfiBE-y---","label":"json","name":"json"},{"_key":"security","_id":"topics/security","_rev":"_VfiBEBy---","label":"security","name":"security"},{"_key":"graphdb","_id":"topics/graphdb","_rev":"_VfiBELu---","label":"graphdb","name":"graphdb"},{"_key":"cloud-computing","_id":"topics/cloud-computing","_rev":"_VfiBDmS---","label":"cloud computing","name":"cloud computing"},{"_key":"css","_id":"topics/css","_rev":"_VfiBEPS---","label":"css","name":"css"},{"_key":"php","_id":"topics/php","_rev":"_VfiBEPO---","label":"php","name":"php"},{"_key":"javascript","_id":"topics/javascript","_rev":"_VfiBEPW---","label":"javascript","name":"javascript"},{"_key":"bear-sunday","_id":"topics/bear-sunday","_rev":"_VfiBD5q---","label":"bear.sunday","name":"bear.sunday"},{"_key":"encryption","_id":"topics/encryption","_rev":"_VfiBDmK---","label":"encryption","name":"encryption"},{"_key":"ci","_id":"topics/ci","_rev":"_VfiBEBm---","label":"ci","name":"ci"},{"_key":"laravel","_id":"topics/laravel","_rev":"_VfiBDuq---","label":"laravel","name":"laravel"},{"_key":"back-end","_id":"topics/back-end","_rev":"_VfiBEIW---","label":"back-end","name":"back-end"},{"_key":"testing-tools","_id":"topics/testing-tools","_rev":"_VfiBDii---","label":"testing tools","name":"testing tools"},{"_key":"privacy","_id":"topics/privacy","_rev":"_VfiBEB6---","label":"privacy","name":"privacy"},{"_key":"coding","_id":"topics/coding","_rev":"_VfiBEGK---","label":"coding","name":"coding"},{"_key":"graph-database","_id":"topics/graph-database","_rev":"_VfiBDmC---","label":"graph database","name":"graph database"},{"_key":"ui-design","_id":"topics/ui-design","_rev":"_VfiBEGO---","label":"ui design","name":"ui design"},{"_key":"mariadb","_id":"topics/mariadb","_rev":"_VfiBDwi--_","label":"mariadb","name":"mariadb"},{"_key":"cd","_id":"topics/cd","_rev":"_VfiBEBq---","label":"cd","name":"cd"},{"_key":"quality","_id":"topics/quality","_rev":"_VfiBEBy--_","label":"quality","name":"quality"}];
 
 const columns = [{
 	id: '1',
 	title: 'Name',
 	dataIndex: 'name',
-}, {
-	id: '2',
-	title: 'Description',
-	className: 'description',
-	dataIndex: 'description',
-}, {
-	id: '3',
-	title: 'Location',
-	dataIndex: 'location',
-}, {
-	id: '4',
-	title: 'Buy ticket',
-	dataIndex: 'url',
 }];
 
 const conferences = [{
@@ -195,7 +210,7 @@ class App extends Component {
 						{ this.state.topics.map( function( topic ) {
 								return (
 									<TopicItem key={ topic.id }>
-										<TopicName>{ topic.name }
+										<TopicName>{ topic.label }
 											<TopicCheckbox
 												name={ topic.name }
 												defaultChecked={ topic.checked }
@@ -216,7 +231,7 @@ class App extends Component {
 			</Topics>
             <Conferences>
                 <SubTitle>We recommend visiting these conferences:</SubTitle>
-				<table>
+				<table width="100%">
 					<tbody>
 					<tr>
 						{ columns.map( function( column ) {
@@ -226,12 +241,11 @@ class App extends Component {
 							}
 						)}
 					</tr>
+					{ console.log( "Conferences", this.state.conferences ) }
 					{ conferences.map( function( conference ){
 						return (
 							<tr key={ conference.id }>
 								<td>{ conference.name }</td>
-								<td>{ conference.location }</td>
-								<td><a href={ conference.url }>Order now</a></td>
 							</tr>
 						)
 					})}
